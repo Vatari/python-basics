3e60cde26a901c31bb195023f3437f20ae4f5896
vatari <majestic13@gmail.com>
2023-07-24 08:17:58 +0300

    exercise

---
+++ first_steps/lab/projects_creation.py
@@ -0,0 +1,7 @@
+name = input('Enter name of the architect: ')
+projects = int(input("Enter number of project/s: "))
+
+
+hours = projects * 3
+
+print(f"The architect {name} will need {hours} hours to complete {projects} project/s.")
