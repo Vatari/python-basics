e1e35e239dbb58f3ead34aa544dbfa504e0b58f1
vatari <majestic13@gmail.com>
2023-08-13 08:33:51 +0300

    exercise

---
+++ nested_loops/exercise/number_pyramid.py
@@ -0,0 +1,15 @@
+num = int(input())
+
+current = 1
+is_current_bigger = False
+
+for row in range(1, num + 1):
+    for col in range(1, row + 1):
+        if current > num:
+            is_current_bigger = True
+            break
+        print(str(current) + " ", end="")
+        current += 1
+    if is_current_bigger:
+        break
+    print()
