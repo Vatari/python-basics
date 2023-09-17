97c3c8c824a9e9c600c4e0bd1c45579d844ccdeb
vatari <majestic13@gmail.com>
2023-09-17 09:57:59 +0300

    exercise

---
+++ first_steps/exercise/basketball_equipment.py
@@ -0,0 +1,11 @@
+annual_fee = int(input())
+
+snickers = annual_fee * 0.6
+dress = snickers * 0.8
+ball = dress / 4
+accessories = ball / 5
+
+
+total_sum = snickers + dress + ball + accessories + annual_fee
+
+print (total_sum)
